feb6cb675f0253e6b974199331815406e9dcda60
andrewpatto <andrew@patto.net>
2022-05-11 15:16:38 +1000

    Make the Docker have a separate builder for node

--- application/fingerprint-docker-image/test-docker/test-check-start-docker.sh
+++ application/fingerprint-docker-image/test-docker/test-check-start-docker.sh
@@ -4,9 +4,9 @@
 SECRETARN="arn:aws:secretsmanager:ap-southeast-2:843407916570:secret:IcaSecretsPortal"  # pragma: allowlist secret
 BN="sandbox-fingerprint-please-remove" # pragma: allowlist secret
 
-docker build --platform linux/amd64 -t fingerprint-fargate ..
+docker build --platform linux/amd64 -t fingerprint ..
 
-docker run --rm --platform linux/amd64  \
+docker run --rm -p 9000:8080 --platform linux/amd64  \
  --env AWS_REGION=ap-southeast-2 \
  --env AWS_ACCESS_KEY_ID \
  --env AWS_SECRET_ACCESS_KEY \
@@ -15,9 +15,7 @@ docker run --rm --platform linux/amd64  \
  --env "FINGERPRINT_BUCKET_NAME=$BN" \
  --mount "type=bind,source=$(pwd)/reference.fasta,target=/tmp/reference.fasta" \
  --mount "type=bind,source=$(pwd)/sites.vcf.gz,target=/tmp/sites.vcf.gz" \
- --entrypoint node \
- fingerprint "/var/task/extract.cjs" \
-  "gds://umccr-research/test_data/CCR180149_tumor_mini.bam" \
+ 843407916570.dkr.ecr.ap-southeast-2.amazonaws.com/cdk-hnb659fds-container-assets-843407916570-ap-southeast-2:fe918c2eb63027859b0e5ffd33885b85f1df4f619bda11cf27ffae849cd9a108 \
+ check-start.lambdaHandler
 
- # "s3://umccr-research-dev/htsget/htsnexus_test_NA12878.bam" \
-
+# docker run -it --entrypoint sh
