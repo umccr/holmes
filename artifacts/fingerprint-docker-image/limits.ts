d31628bb77c7da10484103ff66a0524b189fa7d0
andrewpatto <andrew@patto.net>
2025-11-25 16:27:08 +1100

    add comment

--- artifacts/fingerprint-docker-image/limits.ts
+++ artifacts/fingerprint-docker-image/limits.ts
@@ -1,3 +1,10 @@
 export const MAX_RELATE = 25;
 
+// the limits are mainly designed to avoid overflow of the steps state machines
+// the use of distributed maps solves that in some sense - but we have to be
+// careful that no individual invoke of a lambda returns results into the
+// state that is > 256kb
+// the result of "check" grows roughly linearly with the number of samples
+// in the check. At max check 50 - the result was 56kb. We have now set it
+// to 100 for a trial.
 export const MAX_CHECK = 100;
